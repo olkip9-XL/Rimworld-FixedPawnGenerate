9b14ffa454d7d9fa2a4d2b06941e0b2ec55c6336
olkip9-XL <48114415+olkip9-XL@users.noreply.github.com>
2025-12-25 17:09:49 +0800

    compact 1.5

---
+++ Source/Compact_1.5/Comps/Props/CompProperties_SimplePawn.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace FixedPawnGenerate
+{
+    public class CompProperties_SimplePawn : CompProperties
+    {
+        protected class AlterSimplePawnGraphicData
+        {
+            [NoTranslate]
+            public string tag;
+
+            public GraphicData graphicData;
+        }
+
+        public CompProperties_SimplePawn()
+        {
+            this.compClass = typeof(Comp_SimplePawn);
+        }
+
+        //props
+        public GraphicData graphicData;
+
+        protected List<AlterSimplePawnGraphicData> alterGraphics = new List<AlterSimplePawnGraphicData>();
+
+        public EffecterDef switchEffecter;
+
+        public GraphicData GraphicOfTag(string tag)
+        {
+            if (tag == "default")
+            {
+                return graphicData;
+            }
+
+            foreach (AlterSimplePawnGraphicData alter in alterGraphics)
+            {
+                if (alter.tag == tag)
+                {
+                    return alter.graphicData;
+                }
+            }
+            return graphicData;
+        }
+
+    }
+}
--- Source/Compact_1.5/RenderNode/PawnRenderNode_SimplePawn.cs
+++ Source/Compact_1.5/RenderNode/PawnRenderNode_SimplePawn.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RimWorld;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,10 +18,26 @@ namespace FixedPawnGenerate
 
         public override Graphic GraphicFor(Pawn pawn)
         {
-            Graphic graphic = pawn.def.GetModExtension<ModExtension_SimplePawnData>()?.graphicData?.GraphicColoredFor(pawn);
+            Graphic graphic = BaseContent.BadGraphic;
+
+            Comp_SimplePawn compSimplePawn = pawn.TryGetComp<Comp_SimplePawn>();
+            if (compSimplePawn == null)
+            {
+                Log.Error($"Failed to find Graphic for pawn {pawn.LabelShort}, missing Comp_SimplePawn");
+                return graphic;
+            }
+
+            try
+            {
+                graphic = compSimplePawn.GetCurrentGraphicData().GraphicColoredFor(pawn);
+            }
+            catch (Exception e)
+            {
+                Log.Error($"Failed to get Graphic for pawn {pawn.LabelShort} in Comp_SimplePawn: {e}");
+                return BaseContent.BadGraphic;
+            }
 
             float age = pawn.ageTracker.AgeBiologicalYearsFloat;
-
             if (age < 13f)
             {
                 float drawSizeFactor = 0.5f + (age / 13f) * 0.5f;
